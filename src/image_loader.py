3fe2c91db60f884bbc12a0a20def9626d52d73eb
Maryeon <739759682@qq.com>
2019-12-26 18:56:59 +0800

    add some comments and update labels

---
+++ src/image_loader.py
@@ -0,0 +1,154 @@
+import os
+import csv
+import random
+import pickle
+import torch.utils.data
+import torchvision.transforms as transforms
+import numpy as np
+from meta import *
+from PIL import Image
+from torch.autograd import Variable
+
+
+category = ATTRIBUTES
+category_num = ATTRIBUTES_NUM
+
+
+def make_dataset(num_triplets):
+
+    filename_csv = os.path.join(DATAPATH, DATASET, LABEL_FILE['train'])
+    csvFile = open(filename_csv,'r')
+    reader = csv.reader(csvFile)
+    data = []
+    for item in reader:
+        data.append(item)
+    csvFile.close()
+
+
+    category_dict = {}
+
+    triplets = []
+    for c in category:
+        category_dict[c] = []
+
+    for item in data:
+        if item[2].find('m') == -1:
+            category_dict[item[1].replace('_labels','')].append([item[0], item[2].find('y')])
+
+    for i in range(num_triplets):
+        cate_r = random.randint(0, len(category)-1)
+
+        cate_sub = random.randint(0, category_num[category[cate_r]]-1)
+
+        while True:
+            a = random.randint(0, len(category_dict[category[cate_r]])-1)
+            if category_dict[category[cate_r]][a][1] == cate_sub:
+                break
+
+        while True:
+            b = random.randint(0, len(category_dict[category[cate_r]])-1)
+            if  category_dict[category[cate_r]][b][1] != cate_sub:
+                break
+            
+        while True:
+            c = random.randint(0, len(category_dict[category[cate_r]])-1)
+            if a != c and category_dict[category[cate_r]][c][1] == cate_sub:
+                break
+            
+        triplets.append([category_dict[category[cate_r]][a],category_dict[category[cate_r]][b],category_dict[category[cate_r]][c],cate_r])
+
+    return triplets
+
+
+def default_image_loader(path):
+    return Image.open(path).convert('RGB')
+
+
+class TripletImageLoader(torch.utils.data.Dataset):
+
+    def __init__(self, root, base_path, num_triplets, transform=None,
+                 loader=default_image_loader):
+        self.triplets = make_dataset(num_triplets)
+        self.dataroot = os.path.join(root, base_path)
+        self.loader = loader
+        self.transform = transform
+
+    def __getitem__(self, index):
+        path1 = self.triplets[index][0][0]
+        path2 = self.triplets[index][1][0]
+        path3 = self.triplets[index][2][0]
+        c = self.triplets[index][3]
+
+        if os.path.exists(os.path.join(self.dataroot, path1)):
+            img1 = self.loader(os.path.join(self.dataroot, path1))
+        else:
+            return None
+
+        if os.path.exists(os.path.join(self.dataroot, path2)):
+            img2 = self.loader(os.path.join(self.dataroot, path2))
+        else:
+            return None
+
+        if os.path.exists(os.path.join(self.dataroot, path3)):
+            img3 = self.loader(os.path.join(self.dataroot, path3))
+        else:
+            return None
+
+        if self.transform is not None:
+            img1 = self.transform(img1)
+            img2 = self.transform(img2)
+            img3 = self.transform(img3)
+
+        return img1, img2, img3, c
+
+    def __len__(self):
+        return len(self.triplets)
+
+
+class ImageLoader(torch.utils.data.Dataset):
+    
+    def __init__(self, root, base_path, filenames_filename, split, cand_query, transform=None, loader=default_image_loader):
+        ''' root:                   rootpath to data
+            base_path:              dataset, e.g., fashionAI
+            filenames_filename:     file of image names
+            split:                  valid or test
+            cand_query:             candidate or query
+        '''
+        self.root = root
+        self.base_path = base_path
+        self.filenamelist = []
+        with open(os.path.join(self.root, filenames_filename)) as f:
+            for line in f:
+                self.filenamelist.append(line.rstrip('\n'))
+        samples = []
+        with open(os.path.join(self.root, cand_query+'_'+split+'.txt')) as f:
+            for line in f:
+                samples.append((line.split()[0], int(line.split()[1]), int(line.split()[2])))   #picid condition groundtruth
+        np.random.shuffle(samples)
+        self.samples = samples
+        self.transform = transform
+        self.loader = loader
+
+    def __getitem__(self, index):
+        path, c, gdtruth = self.samples[index]
+        if os.path.exists(os.path.join(self.root, self.base_path, self.filenamelist[int(path)])):
+            img = self.loader(os.path.join(self.root, self.base_path, self.filenamelist[int(path)]))
+            if self.transform is not None:
+                img = self.transform(img)
+            return img, c, gdtruth, self.filenamelist[int(path)].strip().split('/')[-1].split('.')[0]   #imgdata, condition, groundtruth, imgid(for retrieval use)
+        else:
+            return None
+
+    def __len__(self):
+        return len(self.samples)
+
+    def sample(self):
+        #randomly sample two images for each category
+        samples = []
+        for attribute in range(len(category)):
+            sub = [sample for sample in self.samples if sample[1] == attribute]
+            sample = random.sample(sub, 2)
+            samples.append((os.path.join(self.root, self.base_path, self.filenamelist[int(sample[0][0])]), sample[0][1]))
+            samples.append((os.path.join(self.root, self.base_path, self.filenamelist[int(sample[1][0])]), sample[1][1]))
+
+        return samples
---
+++ src/model.py
@@ -0,0 +1,116 @@
+from __future__ import division
+import torch
+import torch.nn as nn
+import numpy as np
+import torch.nn.functional as F
+from torch.nn import init
+
+def l2norm(X):
+    """L2-normalize columns of X
+    """
+    norm = torch.pow(X, 2).sum(dim=1, keepdim=True).sqrt()
+    X = torch.div(X, norm)
+    return X
+
+
+class Tripletnet(nn.Module):
+    def __init__(self, embeddingnet):
+        super(Tripletnet, self).__init__()
+        self.embeddingnet = embeddingnet
+
+    def forward(self, x, y, z, c):
+        """ x: Anchor image,
+            y: Distant (negative) image,
+            z: Close (positive) image,
+            c: Integer indicating according to which notion of similarity images are compared"""
+        embedded_x = self.embeddingnet(x, c)
+        embedded_y = self.embeddingnet(y, c)
+        embedded_z = self.embeddingnet(z, c)
+        sim_a = torch.sum(embedded_x * embedded_y, dim=1)
+        sim_b = torch.sum(embedded_x * embedded_z, dim=1)
+
+        return sim_a, sim_b
+
+
+class ASENet(nn.Module):
+    def __init__(self, backbonenet, embedding_size, n_conditions):
+        super(ASENet, self).__init__()
+        self.backbonenet = backbonenet
+        self.n_conditions = n_conditions
+        self.embedding_size = embedding_size
+
+        self.mask_fc1 = nn.Linear(self.n_conditions, 512, bias=False)
+        self.mask_fc2 = nn.Linear(self.n_conditions, 1024, bias=False)
+        self.fc1 = nn.Linear(2048, 512)
+        self.fc2 = nn.Linear(512, 1024)
+        self.feature_fc = nn.Linear(1024, 1024)
+        self.conv1 = nn.Conv2d(1024, 512, kernel_size=1, stride=1)
+        self.conv2 = nn.Conv2d(512, 1, kernel_size=1, stride=1)
+        self.tanh = nn.Tanh()
+        self.relu = nn.ReLU()
+        self.softmax = nn.Softmax(dim=2)
+        self.sigmoid = nn.Sigmoid()
+        
+    def forward(self, x, task, norm=True):
+        x, _ = self.backbonenet(x)
+
+        img_embedding = self.conv1(x)
+        img_embedding = self.tanh(img_embedding)
+
+        c = task.view(task.size(0), 1).cpu()
+        mask_fc_input = torch.zeros(c.size(0), self.n_conditions).scatter_(1, c, 1)
+        mask_fc_input = mask_fc_input.cuda()
+        mask = self.mask_fc1(mask_fc_input)
+        mask = self.tanh(mask)
+        mask = mask.view(mask.size(0), mask.size(1), 1, 1)
+        mask = mask.expand(mask.size(0), mask.size(1), 14, 14)
+
+        #spatial attention
+        attmap = mask * img_embedding
+        attmap = self.conv2(attmap)
+        attmap = self.tanh(attmap)
+        attmap = attmap.view(attmap.size(0), attmap.size(1), -1)
+        attmap = self.softmax(attmap)
+        attmap = attmap.view(attmap.size(0), attmap.size(1), 14, 14)
+
+        x = x * attmap
+        x = x.view(x.size(0), x.size(1), x.size(2)*x.size(3))
+        x = x.sum(dim=2)
+
+        #channel attention
+        mask = self.relu(self.mask_fc2(mask_fc_input))
+        mask = torch.cat((x, mask), dim=1)
+        mask = self.fc1(mask)
+        mask = self.relu(mask)
+        mask = self.fc2(mask)
+        mask = self.sigmoid(mask)
+        x = x * mask
+        x = self.feature_fc(x)
+
+        if norm:
+            x = l2norm(x)
+
+        return x
+
+    def spatial_heatmaps(self, x, task):
+        feature, _ = self.backbonenet(x)
+
+        img_embedding = self.conv1(feature)
+        img_embedding = self.tanh(img_embedding)
+
+        task = task.view(task.size(0), 1).cpu()
+        mask_fc_input = torch.zeros(task.size(0), self.n_conditions).scatter_(1, task, 1)
+        mask_fc_input = mask_fc_input.cuda()
+        mask = self.mask_fc1(mask_fc_input)
+        mask = self.tanh(mask)
+        mask = mask.view(mask.size(0), mask.size(1), 1, 1)
+        mask = mask.expand(mask.size(0), mask.size(1), 14, 14)
+
+        attmap = mask * img_embedding
+        attmap = self.conv2(attmap)
+        attmap = self.tanh(attmap)
+        attmap = attmap.view(attmap.size(0), attmap.size(1), -1)
+        attmap = self.softmax(attmap)
+        attmap = attmap.view(attmap.size(0), attmap.size(1), 14, 14)
+        attmap = attmap.squeeze()
+        return attmap
