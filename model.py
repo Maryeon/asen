cc673bc3d7508431508abdd1aff7cf61705ea423
Maryeon <739759682@qq.com>
2020-02-12 20:32:55 +0800

    add ASENet_V2

--- model.py
+++ model.py
@@ -32,14 +32,14 @@ class Tripletnet(nn.Module):
 
 
 class ASENet(nn.Module):
-    def __init__(self, backbonenet, embedding_size, n_conditions):
+    def __init__(self, backbonenet, embedding_size, n_attributes):
         super(ASENet, self).__init__()
         self.backbonenet = backbonenet
-        self.n_conditions = n_conditions
+        self.n_attributes = n_attributes
         self.embedding_size = embedding_size
 
-        self.mask_fc1 = nn.Linear(self.n_conditions, 512, bias=False)
-        self.mask_fc2 = nn.Linear(self.n_conditions, 1024, bias=False)
+        self.mask_fc1 = nn.Linear(self.n_attributes, 512, bias=False)
+        self.mask_fc2 = nn.Linear(self.n_attributes, 1024, bias=False)
         self.fc1 = nn.Linear(2048, 512)
         self.fc2 = nn.Linear(512, 1024)
         self.feature_fc = nn.Linear(1024, 1024)
@@ -57,7 +57,7 @@ class ASENet(nn.Module):
         img_embedding = self.tanh(img_embedding)
 
         c = task.view(task.size(0), 1).cpu()
-        mask_fc_input = torch.zeros(c.size(0), self.n_conditions).scatter_(1, c, 1)
+        mask_fc_input = torch.zeros(c.size(0), self.n_attributes).scatter_(1, c, 1)
         mask_fc_input = mask_fc_input.cuda()
         mask = self.mask_fc1(mask_fc_input)
         mask = self.tanh(mask)
@@ -98,7 +98,7 @@ class ASENet(nn.Module):
         img_embedding = self.tanh(img_embedding)
 
         task = task.view(task.size(0), 1).cpu()
-        mask_fc_input = torch.zeros(task.size(0), self.n_conditions).scatter_(1, task, 1)
+        mask_fc_input = torch.zeros(task.size(0), self.n_attributes).scatter_(1, task, 1)
         mask_fc_input = mask_fc_input.cuda()
         mask = self.mask_fc1(mask_fc_input)
         mask = self.tanh(mask)
@@ -113,3 +113,107 @@ class ASENet(nn.Module):
         attmap = attmap.view(attmap.size(0), attmap.size(1), 14, 14)
         attmap = attmap.squeeze()
         return attmap
+
+
+class ASENet_V2(nn.Module):
+    def __init__(self, backbonenet, embedding_size, n_attributes):
+        super(ASENet_V2, self).__init__()
+        self.backbonenet = backbonenet
+        self.n_attributes = n_attributes
+        self.embedding_size = embedding_size
+
+        self.attr_embedding = torch.nn.Embedding(n_attributes, 512)
+
+        self.attr_transform1 = nn.Linear(512, 512)
+        self.conv1 = nn.Conv2d(1024, 512, kernel_size=1, stride=1)
+        self.img_bn1 = nn.BatchNorm2d(512)
+        self.conv2 = nn.Conv2d(512, 1, kernel_size=1, stride=1)
+
+        self.attr_transform2 = nn.Linear(512, 512)
+        self.fc1 = nn.Linear(1536, 512)
+        self.fc2 = nn.Linear(512, 1024)
+
+        self.feature_fc = nn.Linear(1024, self.embedding_size)
+
+        self.tanh = nn.Tanh()
+        self.relu = nn.ReLU(inplace=True)
+        self.softmax = nn.Softmax(dim=2)
+        self.sigmoid = nn.Sigmoid()
+        
+    def forward(self, x, c, norm=True):
+        x = self.backbonenet(x)
+
+        img = self.conv1(x)
+        img = self.img_bn1(img)
+        img = self.tanh(img)
+
+        attr = self.attr_embedding(c)
+        attr = self.attr_transform1(attr)
+        attr = self.tanh(attr)
+        attr = attr.view(attr.size(0), attr.size(1), 1, 1)
+        attr = attr.expand(attr.size(0), attr.size(1), 14, 14)
+
+        #attribute-aware spatial attention
+        attmap = attr * img
+        attmap = self.conv2(attmap)
+        attmap = self.tanh(attmap)
+        attmap = attmap.view(attmap.size(0), attmap.size(1), -1)
+        attmap = self.softmax(attmap)
+        attmap = attmap.view(attmap.size(0), attmap.size(1), 14, 14)
+
+        x = x * attmap
+        x = x.view(x.size(0), x.size(1), x.size(2)*x.size(3))
+        x = x.sum(dim=2)
+
+        #channel attention
+        attr = self.attr_embedding(c)
+        attr = self.attr_transform2(attr)
+        attr = self.relu(attr)
+        img_attr = torch.cat((x, attr), dim=1)
+        mask = self.fc1(img_attr)
+        mask = self.relu(mask)
+        mask = self.fc2(mask)
+        mask = self.sigmoid(mask)
+        x = x * mask
+
+        x = self.feature_fc(x)
+        #x = self.relu(x)
+
+        if norm:
+            x = l2norm(x)
+
+        return x
+
+    def get_heatmaps(self, x, c):
+        x = self.backbonenet(x)
+
+        img = self.conv1(x)
+        img = self.img_bn1(img)
+        img = self.tanh(img)
+
+        attr = self.attr_embedding(c)
+        attr = self.attr_transform1(attr)
+        attr = self.tanh(attr)
+        attr = attr.view(attr.size(0), attr.size(1), 1, 1)
+        attr = attr.expand(attr.size(0), attr.size(1), 14, 14)
+
+        #attribute-aware spatial attention
+        attmap = attr * img
+        attmap = self.conv2(attmap)
+        attmap = self.tanh(attmap)
+        attmap = attmap.view(attmap.size(0), attmap.size(1), -1)
+        attmap = self.softmax(attmap)
+        attmap = attmap.view(attmap.size(0), attmap.size(1), 14, 14)
+
+        attmap = attmap.squeeze()
+
+        return attmap
+
+
+model_dict = {
+    'Tripletnet': Tripletnet,
+    'ASENet': ASENet,
+    'ASENet_V2': ASENet_V2
+}
+def get_model(name):
+    return model_dict[name]
